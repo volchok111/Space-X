e14f434587d2a7661ff9daef4f49ec5cb29c5099
Artem Volkov <artem.volkov@synetech.cz>
2023-09-11 01:40:36 +0200

    Add company screen and data displaying

---
+++ SpaceX/app/src/main/java/com/volchok/space_x/feature/company/presentation/CompanyViewModel.kt
@@ -0,0 +1,32 @@
+package com.volchok.space_x.feature.company.presentation
+
+import androidx.lifecycle.viewModelScope
+import com.volchok.space_x.library.api.domain.ObserveCompanyUseCase
+import com.volchok.space_x.library.api.model.company.CompanyModel
+import com.volchok.space_x.library.memory.company.domain.FetchCompanyUseCase
+import com.volchok.space_x.library.mvvm.presentation.AbstractViewModel
+import com.volchok.space_x.library.use_case.domain.invoke
+import kotlinx.coroutines.launch
+
+class CompanyViewModel(
+    private val fetchCompanyUseCase: FetchCompanyUseCase,
+    private val observeCompanyUseCase: ObserveCompanyUseCase
+): AbstractViewModel<CompanyViewModel.State>(State()) {
+
+    init {
+        viewModelScope.launch {
+            fetchCompanyUseCase()
+        }
+
+        viewModelScope.launch {
+            observeCompanyUseCase().collect {
+                state = state.copy(company = it, loading = false)
+            }
+        }
+    }
+
+    data class State(
+        val company: CompanyModel? = null,
+        val loading: Boolean = true
+    ): AbstractViewModel.State
+}
